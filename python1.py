5986441f7c147a3fe3603623e393faa9ebcef61f
dimadm <dimadm@bk.ru>
2019-03-17 23:00:39 +0300

    First task

---
+++ python1.py
@@ -0,0 +1,26 @@
+import sys
+def arithmetic (x, y, oper):
+	if oper == "+":
+		res = x + y
+	elif oper == "-":
+		res = x - y
+	elif oper == "*":
+		res = x * y
+	elif oper == "/":
+		res = x / y
+	else: res = "Неизвестная операция"
+	return res
+try:
+	x = int(input("Введите первое число: "))
+except ValueError:
+	print ("Вы ввели не цифровое значение, операция не возможна")
+	sys.exit()
+try:	
+	y = int(input("Введите второе число: "))
+except ValueError:
+	print ("Вы ввели не цифровое значение, операция не возможна")
+	sys.exit()
+oper = input("Введите математическую операцию (+-*/): ")
+
+print ("Ответ: ", arithmetic(x, y, oper))
+#My first task
